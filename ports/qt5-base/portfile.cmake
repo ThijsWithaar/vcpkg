5e8a7e9875022b7e18cbb660cebce2dd66a760cc
Thijs Withaar <thijs.withaar@gmail.com>
2023-12-10 19:15:35 +0100

    qt: Fix XBKCommon >= 1.60

--- ports/qt5-base/portfile.cmake
+++ ports/qt5-base/portfile.cmake
@@ -76,8 +76,9 @@ qt_download_submodule(  OUT_SOURCE_PATH SOURCE_PATH
                             patches/fix_angle.patch            # Failed to create OpenGL context for format QSurfaceFormat ...
                             patches/mingw9.patch               # Fix compile with MinGW-W64 9.0.0: Redefinition of 'struct _FILE_ID_INFO'
                             patches/XKB_KEY_dead_lowline.patch # https://bugreports.qt.io/browse/QTBUG-117950
-                            patches/fix-arm64.patch            # Fix corss compile with arm64          
+                            patches/fix-arm64.patch            # Fix corss compile with arm64
                             patches/qmake-arm64.patch          # Fix by Oliver Wolff to support ARM64 hosts on Windows
+                            patches/xkbcommon-160.patch        # Newer XKBCommon misses some defines, from https://gitlab.alpinelinux.org/alpine/aports/-/merge_requests/53933
                     )
 
 # Remove vendored dependencies to ensure they are not picked up by the build
